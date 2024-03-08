d3a9ab7894250fd6aad939bb996fe1e93f4dcbaf
Kerem Yilmaz Guney <evolutionkyg@gmail.com>
2024-03-08 13:56:53 +0300

    Added title and description to ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { MultiSelect } from './components';
+import { MultiSelect } from "./components";
 
 function App() {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <div className='w-[500px]'>
+    <div className="flex min-h-screen justify-center mt-48">
+      <div className="w-[500px]">
+        <div className="flex w-full flex-col text-center">
+          <h1 className="mb-4 text-4xl font-bold">Multi Select Autocomplete</h1>
+          <p className="mb-4">Search characters by typing and navigate with arrow keys or tab. Press Enter to choose, and use backspace or the remove button to deselect.</p>
+        </div>
         <MultiSelect />
       </div>
     </div>
